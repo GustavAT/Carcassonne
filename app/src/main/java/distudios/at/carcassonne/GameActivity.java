513f311f645d57197150fa007bcf205c243abaf1
Simon <simonwegmeyer@yahoo.de>
2018-04-02 14:17:47 +0200

    erweiterter Designentwurf zum Spiel starten (Ohne Funktionen) -> werde in neuem Branch versuchen ein Fragment zum Würfeln in der GameActivity zu implementieren

---
+++ app/src/main/java/distudios/at/carcassonne/GameActivity.java
@@ -0,0 +1,20 @@
+package distudios.at.carcassonne;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Simon on 02.04.2018.
+ * Placeholder view for game activity
+ */
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
