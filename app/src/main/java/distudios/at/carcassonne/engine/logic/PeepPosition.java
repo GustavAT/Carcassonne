d1f09c5b801f9efc1b3c631f6f7ebcf85d18c8bf
Simon <simonwegmeyer@yahoo.de>
2018-06-23 12:24:05 +0200

    canSetOnExistingPeeps

--- app/src/main/java/distudios/at/carcassonne/engine/logic/PeepPosition.java
+++ app/src/main/java/distudios/at/carcassonne/engine/logic/PeepPosition.java
@@ -1,57 +1,74 @@
 package distudios.at.carcassonne.engine.logic;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Simon on 26.04.2018.
  */
 
 public enum PeepPosition {
-    Top,
+    TopLeft, Top, TopRight,
     Left,
     Right,
     Center,
-    Bottom;
+    BottomLeft, Bottom, BottomRight;
 
 
-    public static ArrayList<PeepPosition> getPeepPositions(){
-        ArrayList<PeepPosition> peepPositions = new ArrayList<PeepPosition>();
+    public static List<PeepPosition> getPeepPositions() {
+        List<PeepPosition> peepPositions = new ArrayList<>();
+        peepPositions.add(TopLeft);
         peepPositions.add(Top);
+        peepPositions.add(TopRight);
         peepPositions.add(Left);
         peepPositions.add(Right);
         peepPositions.add(Center);
+        peepPositions.add(BottomLeft);
         peepPositions.add(Bottom);
+        peepPositions.add(BottomRight);
 
         return peepPositions;
     }
 
     public static int fromPosition(PeepPosition p) {
-        if (p == Top) {
+        if (p == TopLeft) {
             return 0;
-        } else if (p == Left) {
+        } else if (p == Top) {
             return 1;
-        } else if (p == Right) {
+        } else if (p == TopRight) {
             return 2;
-        } else if (p == Center) {
+        } else if (p == Left) {
             return 3;
-        } else if (p == Bottom) {
+        } else if (p == Right) {
             return 4;
+        } else if (p == Center) {
+            return 5;
+        } else if (p == BottomLeft) {
+            return 6;
+        } else if (p == Bottom) {
+            return 7;
         }
         return 8;
     }
 
     public static PeepPosition fromInt(int p) {
         if (p == 0) {
-            return Top;
+            return TopLeft;
         } else if (p == 1) {
-            return Left;
+            return Top;
         } else if (p == 2) {
-            return Right;
+            return TopRight;
         } else if (p == 3) {
-            return Center;
+            return Left;
         } else if (p == 4) {
+            return Right;
+        } else if (p == 5) {
+            return Center;
+        } else if (p == 6) {
+            return BottomLeft;
+        } else if (p == 7) {
             return Bottom;
         }
-        return Top;
+        return BottomRight;
     }
 }
